d1f8395185e182b28ee4ed048751d6bd73f8b731
tommymetz <tom@tommetzmedia.com>
2024-12-16 13:57:12 -0700

    chore: linting

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
       <div className="section">
         <h2>Services:</h2>
         <ul id="services">
-          {data?.services.map((service, index) => (
+          {data?.services.map((service: any, index: number) => (
             <li key={index}>
               {service.service[0].text}
             </li>
@@ -57,7 +57,7 @@ function App() {
       <div className="section">
         <h2>Featured Work:</h2>
         <ul id="projects">
-          {data?.projects.map((project, index) => (
+          {data?.projects.map((project: any, index: number) => (
             <li key={index}>
               <a href={project.project_link.url} target="_blank" className="work">
                 {project.project_image.url && <img src={project.project_image.url} />}
@@ -74,7 +74,7 @@ function App() {
           <li>
             <span className="strong">Past/Present:</span><br />
             <div id="ongoing-clients">
-              {data?.ongoing_clients.map((client, index) => (
+              {data?.ongoing_clients.map((client: any, index: number) => (
                 <span key={index}>
                   <a href={client.ongoing_clients_link.url} target="_blank">{client.ongoing_clients_title[0].text}</a>
                   {index < data.ongoing_clients.length - 1 && ', '}
