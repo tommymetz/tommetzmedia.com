8d2616ec1535a52b5fa06d8cb7e5e14c6eb7f6f3
tommymetz <tom@tommetzmedia.com>
2024-12-21 08:44:04 -0700

    chore: three background more

--- src/components/three-background/ThreeBackground.tsx
+++ src/components/three-background/ThreeBackground.tsx
@@ -16,7 +16,14 @@ const BackgroundScene = ({ scrollRef }: { scrollRef: React.RefObject<number> }) 
   const zDepth = 8
   const sphereRadiusRange = 0.1
   const sphereRadiusMin = 0.05
-  const scaleAmt = 0.005
+  const scaleAmt = 0.0025
+
+  const getRandomPosition = () => {
+    const x = Math.random() * xWidth - xWidth / 2
+    const y = Math.random() * yHeight - yHeight / 2
+    const z = Math.random() * zDepth - zDepth / 2
+    return [x, y, z] as [number, number, number] 
+  }
 
   // Animation loop
   useFrame(() => {
@@ -37,6 +44,10 @@ const BackgroundScene = ({ scrollRef }: { scrollRef: React.RefObject<number> }) 
         sphere.scale.x = 1
         sphere.scale.y = 1
         sphere.scale.z = 1
+        const position = getRandomPosition()
+        sphere.position.x = position[0]
+        sphere.position.y = position[1]
+        sphere.position.z = position[2]
       }
     });
   })
@@ -44,12 +55,10 @@ const BackgroundScene = ({ scrollRef }: { scrollRef: React.RefObject<number> }) 
   return (
     <group ref={groupRef} position={[xPos, 0/* overridden above */, zPos]}>
       {[...Array(count)].map((_, i) => {
-        const x = Math.random() * xWidth - xWidth / 2
-        const y = Math.random() * yHeight - yHeight / 2
-        const z = Math.random() * zDepth - zDepth / 2
+        const position = getRandomPosition()
         const sphereRadius = Math.random() * (sphereRadiusRange - sphereRadiusMin) + sphereRadiusMin
         return (
-          <group position={[x, y, z]} key={i}>
+          <group position={position} key={i}>
             <mesh ref={el => el && (sphereRefs.current[i] = el)}>
               <circleGeometry args={[sphereRadius, 20]} />
               <meshStandardMaterial color="lightgray" flatShading={true} />
